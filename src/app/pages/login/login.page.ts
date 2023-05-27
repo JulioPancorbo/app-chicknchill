46d6b2b2fa427d3694f5fcffabb0e3a54cb68792
Julio <julioalbertopancorbo@gmail.com>
2023-05-27 20:16:51 +0200

    Create coer-page and login design

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -39,8 +39,8 @@ export class LoginPage implements OnInit {
     this.menuCtrl.enable(false);
 
     this.form = this.formBuilder.group({
-      email: ['info@xerintel.es', [Validators.required, Validators.email]],
-      password: ['xeripassword', Validators.required]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
     });
   }
 
