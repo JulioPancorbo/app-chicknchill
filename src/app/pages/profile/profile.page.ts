7fd251ef85169aa088f762a77d65368005b9869b
JulioPancorbo <julioalbertopancorbo@gmail.com>
2023-06-07 00:25:14 +0200

    Add profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
 import { User } from 'src/app/models/User';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { codeErrors } from 'src/app/utils/utils';
 import { UtilitiesService } from 'src/app/services/utilities.service';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -14,69 +10,63 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class ProfilePage implements OnInit {
 
-  public user: User;
-  public form: FormGroup;
-  public base64img: string;
-  public isLoading: boolean = true;
+  public user: any = {
+    id: 1,
+    name: 'Penélope',
+    crestas: 4650
+  };
 
-  constructor(private apiService: ApiService,
-    private formBuilder: FormBuilder,
+  public rewards: any[] = [
+    {
+      id: 1,
+      name: "Patatas Chick 'n' Chill",
+      image: "/assets/imgs/nobg/patatas.png",
+      crestas: 3000
+    },
+    {
+      id: 2,
+      name: "Refresco Refill",
+      image: "/assets/imgs/nobg/refresco3.png",
+      crestas: 3600
+    },
+    {
+      id: 3,
+      name: "Hamburguesa de pollo frito",
+      image: "/assets/imgs/nobg/chickenburger.png",
+      crestas: 5000
+    },
+    {
+      id: 4,
+      name: "Chups 'n' Chill",
+      image: "/assets/imgs/nobg/chups5.png",
+      crestas: 4200
+    },
+    {
+      id: 5,
+      name: "Fingers de pollo",
+      image: "/assets/imgs/nobg/fingers.png",
+      crestas: 5000
+    },
+    {
+      id: 6,
+      name: "Tequeños",
+      image: "/assets/imgs/nobg/teques.png",
+      crestas: 3000
+    }
+  ];
+  
+
+  constructor(
     private utilities: UtilitiesService,
-    private camera: Camera,
-    public auth: AuthenticationService) {
-
-  }
+    public auth: AuthenticationService
+  ) {}
 
   public ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      name: [''],
-      email: [''],
-      avatar: ['']
-    });
+    
 
-    this.apiService.getEntity('user').subscribe((user: User) => {
-      this.user = user;
-      this.form.patchValue(user);
-      this.isLoading = false;
-    }, error => {
-      this.utilities.showToast("Error obteniendo el usuario");
-      this.isLoading = false;
-    });
+    
+  }  
 
-  }
-
-  public submitForm(): void {
-    this.apiService.updateUser(this.form.value).subscribe((user: User) => {
-      this.utilities.showToast('Usuario actualizado correctamente');
-    }, (error) => {
-      this.utilities.showToast(codeErrors(error));
-    });
-  }
-
-  /**
-  * Cambiar imagen de perfil
-  */
-  public adjuntarImagen(): void {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      mediaType: this.camera.MediaType.PICTURE,
-      encodingType: this.camera.EncodingType.JPEG,
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      targetWidth: 1920,
-      targetHeight: 1080,
-      allowEdit: true,
-      correctOrientation: true
-    }
-    this.camera.getPicture(options).then((urlFoto) => {
-      this.base64img = 'data:image/jpeg;base64,' + urlFoto;
-      this.user.avatar = this.base64img;
-      this.form.patchValue({ avatar: this.base64img });
-
-      console.log(urlFoto);
-    }).catch(error => {
-      this.utilities.showAlert('Error al obtener imagen', error);
-    })
-  }
+  
 
 }
